60a561c2fe9d3939e5b10f39ef5a6ba74077c1d3
Kilamper <kilamper@gmail.com>
2024-01-19 21:20:11 +0000

    Final State Commit

---
+++ src/main/java/software/ulpgc/kata7/Main.java
@@ -0,0 +1,16 @@
+package software.ulpgc.kata7;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        new JFrame() {
+            {
+                this.setTitle("A Simple App");
+                this.setSize(300, 300);
+                this.setLocationRelativeTo(null);
+                this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+            }
+        };
+    }
+}
